9112d9ce96ad7a33162b5e68213f490a1585f859
Santiago Riveros Salomón <69372887+SantiagoRivSal@users.noreply.github.com>
2023-12-14 11:30:52 -0300

    Update edicion_equipo.go

--- back/model/edicion_equipo.go
+++ back/model/edicion_equipo.go
@@ -2,7 +2,7 @@ package model
 
 type EdicionEquipo struct{
 	Id int `gorm:"primaryKey"`
-	IdEdicion int `gorm:"type:int(150);not null"`
+	IdEdicionTorneo int `gorm:"type:int(150);not null"`
 	IdEquipo int `gorm:"type:int(150);not null"`
 }
 type EdicionEquipos []EdicionEquipo
