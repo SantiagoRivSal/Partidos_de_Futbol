50b7cef95086721f5282ec4494de0ff17fad3177
Santiago Riveros Salomón <69372887+SantiagoRivSal@users.noreply.github.com>
2023-12-14 11:31:03 -0300

    Update partidos.go

--- back/model/partidos.go
+++ back/model/partidos.go
@@ -2,7 +2,7 @@ package model
 
 type Partido struct{
 	Id int `gorm:"primaryKey"`
-	IdEdicion int `gorm:"type:int(150);not null"`
+	IdEdicionTorneo int `gorm:"type:int(150);not null"`
 	IdFase int `gorm:"type:int(150);not null"`
 	IdEquipoLocal int `gorm:"type:int(150);not null"`
 	IdEquipoVisitante int `gorm:"type:int(150);not null"`
