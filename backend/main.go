bc9ca82d462787e28add105014304a251a13f8b7
SantiagoRivSal <santiagosalomon2001@gmail.com>
2024-06-28 21:23:39 -0300

    Cambios minimos

--- backend/main.go
+++ backend/main.go
@@ -3,9 +3,22 @@ package main
 import (
 	"backend/app"
 	"backend/db"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 	db.StartDbEngine()
 	app.StartRoute()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4000" // Puerto por defecto si no está configurado
+	}
+
+	log.Printf("Starting server on port %s...", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
